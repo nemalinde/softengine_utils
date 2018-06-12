91e41e52b4cd589f3934084c9493a31a7b17c3a2
lindelani <lindelani@takealot.com>
2018-06-13 01:49:20 +0200

    the basics are working

--- BulkVouchers/bulk_vouchers.py
+++ BulkVouchers/bulk_vouchers.py
@@ -1,47 +1,69 @@
 from collections import OrderedDict
 
+
 class GeneratePrintingFile(object):
     def __init__(self, filename):
-        # TODO: READ IN FILE
         self.filename = filename
         self.input_file = open(filename, 'r')
 
     def get_file_properties(self):
         input = self.input_file.readlines()
-
         headers = {}
         body = OrderedDict()
 
         counter = 0
-
         for line in input:
             if len(line.split(':')) > 1:
                 headers[line.split(':')[0]] = line.split(':')[1]
             else:
-                # build voucher dictionary
                 row = {}
                 column_list = headers['voucher_fields'].split(',')
                 voucher_values = line.split(',')
+
                 for c in range(len(column_list)):
                     row[column_list[c]] = voucher_values[c]
-
                 body[counter] = row
                 counter += 1
 
-        print headers, body
+        data = self.store_data(column_list, body)
+        self.print_data(data)
 
-    def get_entry(self, field):
-        space = ' '
-        return field + (space * (25 - len(field)))
+    def prepare_row(self, fields):
+        str_ = ''
+        print fields
+        for c in fields:
+            str_ += '{}   '.format(str(c))
 
-    def column_format(self):
+    def store_data(self, column_list, body):
+        data = {}
+        for column_heading in column_list:
+            data[column_heading] = []
 
-        # TODO: the column
-        # TODO: new row (column%number_of_columnd)
-        # have representation of space
-        # have representation of
-        # print every line separately
-        return
+        for entry in body:
+            voucher = body[entry]
+            for row in voucher:
+                data[row].append(voucher[row])
+        return data
+
+    def print_data(self, data):
+        counter = 0
+        num_of_vouchers = len(data['pin'])
+        num_columns = 5
+        temp_counter = 0
+
+        while counter < num_of_vouchers:
+            for i in data:
+                temp_counter = 0
+                doc_row = []
+                voucher_field = data[i]
+                while temp_counter < num_columns:
+                    doc_row.append(voucher_field[counter])
+                    counter += 1
+                    temp_counter += 1
+                counter -= temp_counter
+                self.prepare_row(doc_row)
+
+            counter += temp_counter
 
 c = GeneratePrintingFile('input.txt')
 c.get_file_properties()
